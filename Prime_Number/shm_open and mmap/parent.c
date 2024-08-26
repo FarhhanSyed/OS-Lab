1ebce795781ec6c28379ff1d5d552315115a291b
FarhhanSyed <163603911+FarhhanSyed@users.noreply.github.com>
2024-08-27 01:28:59 +0530

    Update parent.c

--- Prime_Number/shm_open and mmap/parent.c
+++ Prime_Number/shm_open and mmap/parent.c
@@ -1,41 +1,41 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<sys/types.h>
-#include<unistd.h>
-#include<sys/shm.h>
-#include<fcntl.h>
-#include<sys/mman.h>
-#include<string.h>
-int main(int argc,char *argv[])
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/shm.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <string.h>
+int main(int argc, char *argv[])
 {
-    if(argc<3)
+    if (argc < 3)
     {
         printf("minimum of 3 arguments must be passed.\n");
         exit(0);
     }
-    int start=atoi(argv[1]);
-    int end=atoi(agv[2]);
-    if(start<2 || end<start)
+    int start = atoi(argv[1]);
+    int end = atoi(agv[2]);
+    if (start < 2 || end < start)
     {
-        printf("Error inputs:%d %d\n",start,end);
+        printf("Error inputs:%d %d\n", start, end);
         return 0;
     }
-    pid_t pid=fork();
-    if(pid==0)
+    pid_t pid = fork();
+    if (pid == 0)
     {
-        execlp("./child","./child",argv[1],argv[2],NULL);
+        execlp("./child", "./child", argv[1], argv[2], NULL);
         perror("execlp failed\n");
         exit(1);
     }
-    else if(pid>0)
+    else if (pid > 0)
     {
         wait(NULL);
         printf("[PARENT] child finished executing\n");
 
-        int shm_fd=shm_open("VSS",O_RDONLY,0666);
-        void *ptr=mmap(0,4096,PROT_READ,MAP_SHARED,shm_fd,0);
+        int shm_fd = shm_open("VSS", O_RDONLY, 0666);
+        void *ptr = mmap(0, 4096, PROT_READ, MAP_SHARED, shm_fd, 0);
 
-        printf("[PARENT] Printing:%d",(char*)ptr);
+        printf("[PARENT] Printing:%d", (char *)ptr);
         shm_unlink("VSS");
     }
     return 0;
